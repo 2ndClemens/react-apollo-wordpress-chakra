6dfb6fa94002622d823a3d2cbffa88650e028e46
clemens.poblotzki <clemens.poblotzki@centigrade.de>
2020-09-03 23:30:11 +0200

    pages

---
+++ docs/precache-manifest.da61643a0f1fb45d3308c461f2241f61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6ef1cba7f8f71fe3cc6d5b3ebd7a8b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6c32675fa318e371e41",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "bd56f4d6ca2ff1ad69cf",
+    "url": "/static/js/2.173fed88.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/static/js/2.173fed88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6c32675fa318e371e41",
+    "url": "/static/js/main.dcb914c8.chunk.js"
+  },
+  {
+    "revision": "08c1ab7adce8358ab255",
+    "url": "/static/js/runtime-main.e3531632.js"
+  }
+]);
